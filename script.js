2604cd10703460a2ee5eaaf0ceb14fc9e73bc70b
emilyerose <emilyrose0021@gmail.com>
2022-07-28 20:18:33 -0400

    fixed history so that buttons cannot be added twice and if the cities in history > 7, the last one will be deleted

--- script.js
+++ script.js
@@ -9,17 +9,30 @@ btn.on('click', function() {
 //fires on form submit (ie clicking the submit button)
 form.on('submit', function(event) {
     event.preventDefault();
-    let inputCity = $('#citysearch').val()
-    addCityButton(inputCity);
+    let inputEl = $('#citysearch');
+    let inputCity = inputEl.val().trim();
+    //check to see if this city is already in search history
+    if (!$(`#${inputCity}`).length) {
+        //if it is not, add it to the history bar
+        addCityButton(inputCity);
+    }
+    //reset the input field to empty
+    inputEl.val('');
 })
 
 function addCityButton(cityName){
     //adds new city button below the form (in that column though)
-    var formContainer = $('.form-container');
+    var historyContainer = $('.history');
     var newBtn = $('<button>');
     newBtn.addClass('btn btn-secondary');
     //sets id to the name of the city
     newBtn.attr('id',cityName);
     newBtn.text(cityName);
-    formContainer.append(newBtn); 
+    historyContainer.prepend(newBtn); 
+
+    //count cities in recent history. if>7, delete the first one
+    let buttonElems = historyContainer.children();
+    if (buttonElems.length>7) {
+        buttonElems.last().remove();
+    }
 }
